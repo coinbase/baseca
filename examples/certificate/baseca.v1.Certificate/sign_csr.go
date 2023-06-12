0b80a2c4a2488a02ef002b81b626b9ba5018b468
Kenneth Yang <kenneth.yang@coinbase.com>
2023-06-12 10:35:15 -0700

    v0.0.1-beta (#2)

---
+++ examples/certificate/baseca.v1.Certificate/sign_csr.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/x509"
+	"fmt"
+	"log"
+
+	baseca "github.com/coinbase/baseca/pkg/client"
+)
+
+func main() {
+	client_id := "[CLIENT_ID]"
+	client_token := "[CLIENT_TOKEN]"
+
+	configuration := baseca.Configuration{
+		URL:         "localhost:9090",
+		Environment: baseca.Env.Local,
+	}
+
+	client, err := baseca.LoadDefaultConfiguration(configuration, client_id, client_token, baseca.Attestation.Local)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	metadata := baseca.CertificateRequest{
+		CommonName:            "sandbox.coinbase.com",
+		SubjectAlternateNames: []string{"sandbox.coinbase.com"},
+		SigningAlgorithm:      x509.SHA384WithRSA,
+		PublicKeyAlgorithm:    x509.RSA,
+		KeySize:               4096,
+		Output: baseca.Output{
+			PrivateKey:                "/tmp/private.key",
+			Certificate:               "/tmp/certificate.crt",
+			CertificateChain:          "/tmp/certificate_chain.crt",
+			CertificateSigningRequest: "/tmp/certificate_request.csr",
+		},
+	}
+
+	response, err := client.IssueCertificate(metadata)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%+v", response)
+}
